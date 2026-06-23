ad4011382addcc8fd97687623f78deb08a39cd60
Faizal Indra N <2503010020@unper.ac.id>
2026-06-23 12:07:03 +0700

    Menambahkan fitur switch case 1 dan 2 oleh Faizal

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,24 @@ using namespace std;
 
 int main(){
 
+        switch (pilihan) {
+            case 1:
+                hotel.tampilkanKamar();
+                break;
 
+            case 2: {
+                Tamu tamuBaru;
+                cout << "\n--- Tambah Antrean Tamu ---\n";
+                cout << "Nama Tamu        : ";
+                cin.ignore();
+                getline(cin, tamuBaru.namaTamu);
+                cout << "No. Identitas/KTP: ";
+                getline(cin, tamuBaru.nomorIdentitas);
+
+                antreanLobi.push(tamuBaru);
+                cout << "[Sukses] " << tamuBaru.namaTamu << " telah ditambahkan ke antrean lobi.\n";
+                break;
+            }
 
     return 0;
 }
